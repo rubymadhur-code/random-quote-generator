5aba1c2d5b4f6a9cce212bfacf26258ef5c3e10d
rubymadhur-code <madhurruby09@gmail.com>
2025-08-30 12:16:17 +0530

    add display randon quote feature

--- src/Componenets/Quotes.jsx
+++ src/Componenets/Quotes.jsx
@@ -1,5 +1,33 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
 
 export default function Quotes(){
+    const [quotesList, setQuotesList] = useState([]);
+    const [ currentQuotes, setCurrentQuotes] = useState({ text:"", author:""});
+
+    useEffect(() => {
+        axios.get("https://dummyjson.com/quotes").then(res => {
+            setQuotesList(res.data.quotes);
+            getRandomQuote(res.data.quotes);
+        });
+    },[]);
+
+    const getRandomQuote = ( QuotesArray = quotesList) => {
+        const randomIndex = Math.floor(Math.random() * QuotesArray.length);
+        const randomQuote = QuotesArray[randomIndex];
+        setCurrentQuotes({text: randomQuote.quote, author:randomQuote.author});
+    }
+
+    const handleCopyQuote = (e) => {
+        e.stopPropagation();
+        if(!currentQuotes) return;
+        const textToCopy = `"${currentQuotes.text}" - ${currentQuotes.author}`;
+        navigator.clipboard.writeText(textToCopy);
+        setCopied(true);
+        setTimeout(()=> setCopied(false),1000);        
+    }
+
+
     return(
 <div className="relative min-h-screen w-full bg-[url('/bg1.webp')] bg-cover bg-center flex flex-col items-center justify-center">
   
@@ -8,24 +36,24 @@ export default function Quotes(){
     
     {/* Quote */}
     <p className="text-2xl font-semibold text-white drop-shadow-lg">
-      "The best way to predict the future is to create it."
+      "{currentQuotes.text}"
     </p>
 
     {/* Author */}
     <span className="mt-3 text-lg text-gray-200 italic">
-      — Peter Drucker
+      — {currentQuotes.author}
     </span>
   </div>
 
   {/* Buttons under circle */}
   <div className="mt-6 flex gap-4">
     {/* Regenerate */}
-    <button className="px-5 py-2 rounded-full bg-white/20 backdrop-blur-md text-white font-medium shadow-lg hover:bg-white/30 transition">
+    <button onClick={() => getRandomQuote()} className="px-5 py-2 rounded-full bg-white/20 backdrop-blur-md text-white font-medium shadow-lg hover:bg-white/30 transition">
       🔄 Regenerate
     </button>
     
     {/* Copy */}
-    <button className="px-5 py-2 rounded-full bg-white/20 backdrop-blur-md text-white font-medium shadow-lg hover:bg-white/30 transition">
+    <button onClick={handleCopyQuote} className="px-5 py-2 rounded-full bg-white/20 backdrop-blur-md text-white font-medium shadow-lg hover:bg-white/30 transition">
       📋 Copy
     </button>
   </div>
